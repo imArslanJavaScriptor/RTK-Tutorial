8d944caf0dbfeee2fddebd5e137160e4f591d219
Ahmad Raza <imarslan7061@gmail.com>
2025-07-30 19:02:22 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+
 function App() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-gray-900">
-      <h1 className="text-xl font-medium text-white">Task Manager App</h1>
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-900 flex-col">
+      <h1 className="text-4xl font-bold text-white">Task Manager App</h1>
+      <AddTask />
+      <TaskList />
     </div>
   );
 }
--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const taskSlice = createSlice({
   name: "task",
@@ -11,9 +11,30 @@ const taskSlice = createSlice({
       reducer: (state, action) => {
         state.items.push(action.payload);
       },
+      prepare: (text) => {
+        return {
+          payload: {
+            id: nanoid(),
+            text,
+            completed: false,
+          },
+        };
+      },
     },
-    toggleTask: {},
-    deleteTask: {},
-    setFilter: {},
+    toggleTask: (state, action) => {
+      const task = state.items.find((t) => t.id === action.payload);
+      if (task) task.completed = !task.completed;
+    },
+
+    deleteTask: (state, action) => {
+      state.items = state.items.filter((t) => t.id !== action.payload);
+    },
+
+    setFilter: (state, action) => {
+      state.filters = action.payload;
+    },
   },
 });
+
+export const { addTask, toggleTask, deleteTask, setFilter } = taskSlice.actions;
+export default taskSlice.reducer;
