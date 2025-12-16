2d1adc426a80500cf9976429a57ba9f84c1e1e81
Muhammad Arslan <imarslan7061@gmail.com>
2025-12-17 00:54:04 +0500

    upd

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "../features/task/taskSlice";
+import { todoSlice } from "../slices/todo/todoSlice.js";
 
 export const store = configureStore({
   reducer: {
-    tasks: taskReducer,
-  },
-});
+    todoSlice
+  }
+})
